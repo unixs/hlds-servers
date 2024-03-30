1338a31255be0840cd7ffba1651fec1bf81a77e6
Alexander Feodorov <vester@unixcomp.org>
2024-03-31 03:53:35 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,7 @@ set -e
 docker compose ls
 docker compose -f servers.yml pull
 docker compose -f servers.yml stop
-docker compose -f servers.yml rm
+docker compose -f servers.yml rm -f
 docker compose -f servers.yml up -d
 docker compose ls
 docker image prune -f
-
